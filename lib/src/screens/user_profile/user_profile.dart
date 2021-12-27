768387c5c956569f05cfc41c08c437ad0180f0ff
kumawatlakhan07072001@gmail.com <i@mMCoo7>
2021-12-27 07:40:31 +0530

    ✔️User Profile Logic

--- lib/src/screens/user_profile/user_profile.dart
+++ lib/src/screens/user_profile/user_profile.dart
@@ -7,26 +7,27 @@ class UserProfile extends StatelessWidget {
   const UserProfile({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    final _scaffoldKey = GlobalKey<ScaffoldState>();
-    return BaseView<UserProfileViewModel>(builder: (context, model, child) {
-      return Scaffold(
-        key: _scaffoldKey,
-        appBar: AppBar(
-          leading: Icon(
-            Icons.arrow_back_outlined,
-            color: Theme.of(context).primaryColorDark,
-          ),
-          elevation: 0,
-          title: Center(
-            child: Text(
-              'My Profile',
-              style: Theme.of(context).textTheme.headline2,
+    return BaseView<UserProfileViewModel>(
+        onModelReady: (model) => model.init(),
+        builder: (context, model, child) {
+          return Scaffold(
+            key: model.scaffoldkey,
+            appBar: AppBar(
+              leading: Icon(
+                Icons.arrow_back_outlined,
+                color: Theme.of(context).primaryColorDark,
+              ),
+              elevation: 0,
+              title: Center(
+                child: Text(
+                  'My Profile',
+                  style: Theme.of(context).textTheme.headline2,
+                ),
+              ),
+              backgroundColor: Theme.of(context).scaffoldBackgroundColor,
             ),
-          ),
-          backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-        ),
-        body: Body(context, model),
-      );
-    });
+            body: Body(context, model),
+          );
+        });
   }
 }
--- lib/view/user_profile_viewmodel.dart
+++ lib/view/user_profile_viewmodel.dart
@@ -1,5 +1,10 @@
+import 'dart:convert';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:ride_sharing/provider/base_model.dart';
+import 'package:ride_sharing/src/models/userprofile_model.dart';
 
 class UserProfileViewModel extends BaseModel {
   final TextEditingController nameController = TextEditingController();
@@ -8,6 +13,11 @@ class UserProfileViewModel extends BaseModel {
   final TextEditingController genderController = TextEditingController();
   final TextEditingController ageController = TextEditingController();
   final TextEditingController addressController = TextEditingController();
+
+  final FirebaseFirestore db = FirebaseFirestore.instance;
+  final FirebaseAuth auth = FirebaseAuth.instance;
+
+  final GlobalKey<ScaffoldState> scaffoldkey = GlobalKey<ScaffoldState>();
 
   void clear() {
     nameController.clear();
@@ -18,12 +28,49 @@ class UserProfileViewModel extends BaseModel {
     notifyListeners();
   }
 
-  void save() {
+  void init() async {
+    String? uid = auth.currentUser?.uid;
+    if (uid != null) {
+      var data = await db.collection('users').doc(uid).get();
+
+      UserProfileModel user =
+          userProfileModelFromJson(json.encode(data.data()));
+      print(user.name);
+      print(user.id);
+      print(user.email);
+      nameController.text = user.name ?? '';
+      phoneController.text = user.phone ?? '';
+      emailController.text = user.email ?? '';
+      genderController.text = user.gender ?? '';
+      ageController.text = user.age ?? '';
+      addressController.text = user.address ?? '';
+      notifyListeners();
+    }
+  }
+
+  void save(BuildContext context) {
     print(nameController.text);
     print(phoneController.text);
     print(emailController.text);
     print(genderController.text);
     print(ageController.text);
     print(addressController.text);
+
+    String? uid = auth.currentUser?.uid;
+
+    if (uid != null) {
+      db.collection("users").doc(uid).update({
+        "name": nameController.text,
+        "phone": phoneController.text,
+        "email": emailController.text,
+        "address": addressController.text,
+        "age": ageController.text,
+        "gender": genderController.text
+      }).then((value) {
+        print("User saved to db");
+        Navigator.of(context).pop();
+        //Navigator.pushNamedAndRemoveUntil(context, "/", (route) => false);
+      });
+    }
   }
 }
