05280dd7e0c44d73787a374e3f9fdbb3eb3fbc24
shivanigu <shivanig.ug20.ece@nitp.ac.in>
2021-12-28 09:41:07 +0530

    Available_drivers

--- lib/src/screens/available_Drivers.dart
+++ lib/src/screens/available_Drivers.dart
@@ -29,6 +29,8 @@ class _AvailableRidersState extends State<AvailableRiders> {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
+    double height = MediaQuery.of(context).size.height;
     return Scaffold(
         appBar: AppBar(
           elevation: 0,
@@ -82,17 +84,36 @@ class _AvailableRidersState extends State<AvailableRiders> {
                 ],
               ),
             ),
-            Center(
-              child: Padding(
-                padding: const EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(8),
+                      topRight: Radius.circular(8),
+                      bottomLeft: Radius.circular(8),
+                      bottomRight: Radius.circular(8)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 2,
+                      blurRadius: 4,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                height: height / 3.0,
                 child: Card(
                   child: Column(
                     children: [
                       Row(
                         children: [
-                          Image.asset(
-                            'assets/images/user_img.png',
-                            width: 80.0,
+                          Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Image.asset(
+                              'assets/images/user_img.png',
+                              height: height / 8.5,
+                            ),
                           ),
                           SizedBox(
                             width: 10.0,
@@ -102,15 +123,16 @@ class _AvailableRidersState extends State<AvailableRiders> {
                               Text(
                                 'Mr. Thangabali',
                                 style: TextStyle(
-                                    fontSize: 25.0,
-                                    fontWeight: FontWeight.w500),
+                                    fontSize: height / 35,
+                                    fontWeight: FontWeight.w400),
                               ),
                               Text(
                                 'Tata Indica',
+                                textAlign: TextAlign.start,
                                 style: TextStyle(
                                     color: config.ThemeColors
                                         .mainTextSecondaryColor(1),
-                                    fontSize: 25.0,
+                                    fontSize: height / 35,
                                     fontWeight: FontWeight.w300),
                               ),
                               Text(
@@ -118,18 +140,167 @@ class _AvailableRidersState extends State<AvailableRiders> {
                                 style: TextStyle(
                                     color: config.ThemeColors
                                         .mainTextSecondaryColor(1),
-                                    fontSize: 25.0,
+                                    fontSize: height / 35,
                                     fontWeight: FontWeight.w300),
                               )
                             ],
-                          )
+                          ),
                         ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            'From : Patliputra, Patna',
+                            style: TextStyle(
+                              color:
+                                  config.ThemeColors.mainTextSecondaryColor(1),
+                            ),
+                          ),
+                          SizedBox(
+                            height: height * 0.005,
+                          ),
+                          Text(
+                            'To : Mahendru Ghat, Patna',
+                            style: TextStyle(
+                              color:
+                                  config.ThemeColors.mainTextSecondaryColor(1),
+                            ),
+                          ),
+                          SizedBox(height: height * 0.005),
+                          Text(
+                            'On 8th Dec,2021 at 8:30 A.M.',
+                            style: Theme.of(context).textTheme.bodyText2,
+                          ),
+                          SizedBox(height: height * 0.005),
+                        ],
+                      ),
+                      FractionallySizedBox(
+                        widthFactor: 0.8,
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Text('See Details'),
+                          color: config.ThemeColors().mainColor(1),
+                          textColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(6.0),
+                          ),
+                        ),
                       )
                     ],
                   ),
                 ),
               ),
-            )
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(8),
+                      topRight: Radius.circular(8),
+                      bottomLeft: Radius.circular(8),
+                      bottomRight: Radius.circular(8)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Colors.grey.withOpacity(0.3),
+                      spreadRadius: 2,
+                      blurRadius: 4,
+                      offset: Offset(0, 3), // changes position of shadow
+                    ),
+                  ],
+                ),
+                height: height / 3,
+                child: Card(
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(15.0),
+                            child: Image.asset(
+                              'assets/images/user_img.png',
+                              height: height / 8.5,
+                            ),
+                          ),
+                          SizedBox(
+                            width: 10.0,
+                          ),
+                          Column(
+                            children: [
+                              Text(
+                                'Mr. Thangabali',
+                                style: TextStyle(
+                                    fontSize: height / 35,
+                                    fontWeight: FontWeight.w400),
+                              ),
+                              Text(
+                                'Tata Indica',
+                                style: TextStyle(
+                                    color: config.ThemeColors
+                                        .mainTextSecondaryColor(1),
+                                    fontSize: height / 35,
+                                    fontWeight: FontWeight.w300),
+                              ),
+                              Text(
+                                '3.5 Stars',
+                                style: TextStyle(
+                                    color: config.ThemeColors
+                                        .mainTextSecondaryColor(1),
+                                    fontSize: height / 35,
+                                    fontWeight: FontWeight.w300),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            'From : Patliputra, Patna',
+                            style: TextStyle(
+                              color:
+                                  config.ThemeColors.mainTextSecondaryColor(1),
+                            ),
+                          ),
+                          SizedBox(
+                            height: height * 0.005,
+                          ),
+                          Text(
+                            'To : Mahendru Ghat, Patna',
+                            style: TextStyle(
+                              color:
+                                  config.ThemeColors.mainTextSecondaryColor(1),
+                            ),
+                          ),
+                          SizedBox(
+                            height: height * 0.005,
+                          ),
+                          Text(
+                            'On 8th Dec,2021 at 8:30 A.M.',
+                            style: Theme.of(context).textTheme.bodyText2,
+                          ),
+                          SizedBox(
+                            height: height * 0.005,
+                          ),
+                        ],
+                      ),
+                      FractionallySizedBox(
+                        widthFactor: 0.8,
+                        child: FlatButton(
+                          onPressed: () {},
+                          child: Text('See Details'),
+                          color: config.ThemeColors().mainColor(1),
+                          textColor: Colors.white,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(6.0),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
           ],
         ));
   }
