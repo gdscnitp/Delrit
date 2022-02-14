5a9e9ff960a8abe4ecc25bddf66c35c907103960
Sumit Kumar <sumit.sav1136@gmail.com>
2022-02-14 17:10:09 +0530

    Dropdown for gender in complete profile and user profile

--- lib/src/screens/complete_profile/components/body.dart
+++ lib/src/screens/complete_profile/components/body.dart
@@ -6,7 +6,7 @@ import 'package:ride_sharing/view/complete_profile_viewmodel.dart';
 class Body extends StatelessWidget {
   final CompleteProfileViewModel model;
   Body({Key? key, required this.model}) : super(key: key);
-  final _formKey=GlobalKey<FormState>();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -30,38 +30,93 @@ class Body extends StatelessWidget {
                 SizedBox(
                   height: App(context).appHeight(4),
                 ),
-                inputFormField(label: 'Name', controller: model.nameController,Ktype:TextInputType.name,error_msg: "mention your name please*"),
-                SizedBox(
-                  height: App(context).appHeight(4),
-                ),
-                inputFormField(label: 'Email', controller: model.emailController,Ktype:TextInputType.emailAddress,error_msg: "mention your email please*"),
-                SizedBox(
-                  height: App(context).appHeight(4),
-                ),
-                inputFormField(label: 'Phone', controller: model.phoneController,Ktype:TextInputType.phone,error_msg: "mention your phone no. please*"),
+                inputFormField(
+                    label: 'Name',
+                    controller: model.nameController,
+                    Ktype: TextInputType.name,
+                    error_msg: "mention your name please*"),
                 SizedBox(
                   height: App(context).appHeight(4),
                 ),
                 inputFormField(
-                    label: 'Gender', controller: model.genderController,Ktype:TextInputType.text,error_msg: "mention your gender please*"),
+                    label: 'Email',
+                    controller: model.emailController,
+                    Ktype: TextInputType.emailAddress,
+                    error_msg: "mention your email please*"),
                 SizedBox(
                   height: App(context).appHeight(4),
                 ),
-                inputFormField(label: 'Age', controller: model.ageController,Ktype:TextInputType.number,error_msg: "mention your age please*"),
+                inputFormField(
+                    label: 'Phone',
+                    controller: model.phoneController,
+                    Ktype: TextInputType.phone,
+                    error_msg: "mention your phone no. please*"),
+                SizedBox(
+                  height: App(context).appHeight(4),
+                ),
+                DropdownButtonFormField<String>(
+                  decoration: InputDecoration(
+                    labelText: "Gender",
+                    filled: true,
+                    fillColor: Colors.grey[300],
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
+                      borderSide: BorderSide(
+                        color: Colors.grey.shade400,
+                        width: 2,
+                      ),
+                    ),
+                    focusedBorder: const OutlineInputBorder(
+                      borderRadius: BorderRadius.all(
+                        Radius.circular(10.0),
+                      ),
+                      borderSide: BorderSide(
+                        color: Colors.black54,
+                        width: 3,
+                      ),
+                    ),
+                    contentPadding: const EdgeInsets.all(15),
+                    // hintText: hint,
+                    labelStyle: const TextStyle(
+                      color: Colors.black54,
+                      backgroundColor: Colors.white,
+                      fontSize: 20,
+                    ),
+                    floatingLabelBehavior: FloatingLabelBehavior.always,
+                  ),
+                  items: <String>['Male', 'Female'].map((String value) {
+                    return DropdownMenuItem<String>(
+                      child: Text(value),
+                      value: value,
+                    );
+                  }).toList(),
+                  onChanged: (val) {
+                    if (val != null) {
+                      model.setGender(val);
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: App(context).appHeight(4),
+                ),
+                inputFormField(
+                    label: 'Age',
+                    controller: model.ageController,
+                    Ktype: TextInputType.number,
+                    error_msg: "mention your age please*"),
                 SizedBox(
                   height: App(context).appHeight(4),
                 ),
                 ElevatedButton(
                   onPressed: () {
                     if (_formKey.currentState!.validate()) {
-                       model.save(context);
-
-                      
+                      model.save(context);
                     } else {
                       return;
                     }
                   },
-                  
                   style: ElevatedButton.styleFrom(
                     padding: EdgeInsets.symmetric(
                       horizontal: App(context).appWidth(20.0),
